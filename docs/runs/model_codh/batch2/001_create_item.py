adfb38cccd8638cb220838f570ad26274cb835b8
nakamura196 <na.kamura.1263@gmail.com>
2022-01-11 14:33:53 +0900

    feat: add prev/next

--- docs/runs/model_codh/batch2/001_create_item.py
+++ docs/runs/model_codh/batch2/001_create_item.py
@@ -4,7 +4,8 @@ koui_path = "../../../../ndl.json"
 
 with open(koui_path) as f:
     df = json.load(f)
-    for obj in df:
+    for i in range(len(df)):
+        obj = df[i]
         page = str(obj["page"]).zfill(4)
 
         item_path = "../item/" + page + ".json"
@@ -12,8 +13,18 @@ with open(koui_path) as f:
         with open(item_path) as f:
             obj2 = json.load(f)
 
-        del obj2["image"]
-        obj2["label"] = obj["label"]
+        try:
+            obj2["prev"] = str(df[i - 1]["page"]).zfill(4)
+        except:
+            pass
+
+        try:
+            obj2["next"] = str(df[i + 1]["page"]).zfill(4)
+        except:
+            pass
+
+        # del obj2["image"]
+        # obj2["label"] = obj["label"]
 
         with open(item_path, 'w') as outfile:
             json.dump(obj2, outfile, ensure_ascii=False,
