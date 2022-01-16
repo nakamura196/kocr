817e47865f0b494b57e24e35bb0c9e82cc56708e
nakamura196 <na.kamura.1263@gmail.com>
2022-01-16 23:33:56 +0900

    feat: update items

--- docs/runs/model_codh/batch3/002_addMissings.py
+++ docs/runs/model_codh/batch3/002_addMissings.py
@@ -2,6 +2,19 @@ import json
 import glob
 import os
 import gzip
+import datetime
+import pathlib
+
+import argparse    # 1. argparseをインポート
+
+parser = argparse.ArgumentParser(description='このプログラムの説明（なくてもよい）')    # 2. パーサを作る
+
+# 3. parser.add_argumentで受け取る引数を追加していく
+parser.add_argument('--date', "-d", default=None)
+
+args = parser.parse_args()    # 4. 引数を解析
+
+date = args.date
 
 file = "../data/bib.json"
 
@@ -21,13 +34,23 @@ arr = []
 for target in df:
     files = glob.glob("../output/{}-*".format(target))
 
-    arr.append("echo '{}'".format(target))
+    arr.append("echo ' - {}'".format(target))
 
     for file in sorted(files):
-        # print(file)
+
+        if date:
+            # print(file)
+            p = pathlib.Path(file)
+            dt = datetime.datetime.fromtimestamp(p.stat().st_ctime)
+
+            dt = dt.split(" ")[0]
+
+            if dt < date:
+                continue
 
         id = file.split("/")[-1]
-
+        
+        arr.append("echo ' -- {}'".format(id))
         arr.append("python 001_create_map.py '{}' '{}' '{}'".format(id, map[target]["attribution"], map[target]["name"]))
         arr.append("python 002_calc.py {}".format(id))
         arr.append("python 003_calc_line.py {}".format(id))
